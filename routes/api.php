fc6bd67d6651e1b80dd72be5148c417d702b5c20
mercy-mwai <mercyneemam@gmail.com>
2025-09-05 12:24:12 +0300

    updates

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,8 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::prefix('auth')->group(function(){
+Route::get('/google',[AuthController::class, 'redirect'])->name('google-auth');
+Route::get('/auth/google/callback', [AuthController::class, 'callbackGoogle']);
 Route::post('/register',[AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
