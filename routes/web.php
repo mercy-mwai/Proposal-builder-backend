5831209be9b0948ec4e8020f03a900aa077d098d
mercy-mwai <mercyneemam@gmail.com>
2025-09-10 09:30:38 +0300

    updates

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('auth')->group(function(){
+Route::get('/google',[AuthController::class, 'redirect'])->name('google-auth');
+Route::get('/google/callback', [AuthController::class, 'callbackGoogle']);
 });
