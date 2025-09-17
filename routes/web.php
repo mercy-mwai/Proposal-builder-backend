0759e0dec761f394c2bcd808206ab169ba40faf7
mercy-mwai <mercyneemam@gmail.com>
2025-09-17 10:27:21 +0300

    blade template

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::get('/accounting-proposal', function () {
-    return view('proposals.templates.accountingProposalTemplate.AccountingProposalTemplate', [
-        '$client_name' => 'Jane Pendo',
-        '$user_assigned'=> 'Nillah Carter',
-        '$company_name'=> 'Starlex',
-        '$user_phone' => '7687897290124',
-        '$user_email' => 'nillah@gmail.com',
-        '$company_address' => 'P.O. Box 12345-00100',
-        '$company_city'=> 'Nairobi',
-        '$company_street'=> 'Upperhill Longonot Road',
-        '$company_zip_code'=> '00010',
-        '$company_url' => 'www.starlextechstore.co.ke',
-
-    ]);
+    return view('proposals.templates.accountingProposalTemplate.AccountingProposalTemplate');
 });
 
 
