f6773485af06ed125fb5d429ff0d6e49feb7d913
mercy-mwai <mercyneemam@gmail.com>
2025-09-19 04:54:24 +0300

    moved businessConsultingTemplate and accountingProposalTemplate to business folder

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::get('/accounting-proposal', function () {
-    return view('proposals.templates.accountingProposalTemplate.AccountingProposalTemplate');
+    return view('proposals.templates.businessProposals.accountingProposalTemplate.AccountingProposalTemplate');
 });
 Route:: get('/business-proposal', function (){
-    return view('proposals.templates.businessConsultingTemplate.businessConsultingTemplate');
+    return view('proposals.templates.businessProposals.businessConsultingTemplate.businessConsultingTemplate');
 });
 
 Route::prefix('auth')->group(function(){
