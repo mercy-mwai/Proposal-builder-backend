259b4e302d047e6c2e3147f10fd5a2bf767b173c
mercy-mwai <mercyneemam@gmail.com>
2025-09-22 12:03:01 +0300

    deleted all the blade templates

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 
-Route::get('/accounting-proposal', function () {
-    return view('proposals.templates.businessProposals.accountingProposalTemplate.AccountingProposalTemplate');
-});
-Route:: get('/business-proposal', function (){
-    return view('proposals.templates.businessProposals.businessConsultingTemplate.businessConsultingTemplate');
-});
-Route::get('/digital-marketing', function(){
-return view('proposals.templates.businessProposals.digitalMarketing.digitalMarketing');
-});
 
 Route::prefix('auth')->group(function(){
 Route::get('/google',[AuthController::class, 'redirect'])->name('google-auth');
