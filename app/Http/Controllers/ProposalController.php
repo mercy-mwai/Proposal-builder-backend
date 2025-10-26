c69be45930c1ad57dd13f1669851d74ce2085d6d
mercy-mwai <mercyneemam@gmail.com>
2025-10-26 16:55:31 +0300

    proposal controller update

--- app/Http/Controllers/ProposalController.php
+++ app/Http/Controllers/ProposalController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class ProposalController extends Controller
 {
-    //
+    public function store(Request $request){
+        $proposal= $request->validate([
+            'template'=>'required|string',
+            'businessInfo.companyName'=>'required|string',
+            'businessInfo.contactPerson'=>'required|string',
+            'businessInfo.email'=>'required|email',
+            'businessInfo.phone'=>'required|phone',
+            'businessInfo.title'=>'required|string',
+            'objectives.items'=>'required|array',
+            'budgets.items'=>'required|array',
+            'timeline.startDate'=>'required|date',
+            'timeline.endDate'=>'required|date',
+            'timeline.milestones'=>'required|date'
+        ]);
+    }
 }
